917fcc9811f5a1e9167b7bde21aa88a38b380890
superrino130 <60283445+superrino130@users.noreply.github.com>
2023-09-13 23:46:56 +0900

    samplea04/a05 add ruby

--- Ruby/samplea04.rb
+++ Ruby/samplea04.rb
@@ -1,2 +1,17 @@
 # https://www.fe-siken.com/kakomon/sample20220425/b4.html
 
+def transformSparseMatrix(matrix)
+  sparseMatrix = [[], [], []]
+  matrix.size.times do |i|
+    matrix[0].size.times do |j|
+      if matrix[i][j] != 0
+        sparseMatrix[0] << i + 1
+        sparseMatrix[1] << j + 1
+        sparseMatrix[2] << matrix[i][j]
+      end
+    end
+  end
+  sparseMatrix
+end
+
+pp transformSparseMatrix([[3, 0, 0, 0, 0], [0, 2, 2, 0, 0], [0, 0, 0, 1, 3], [0, 0, 0, 2, 0], [0, 0, 0, 0, 1]])
--- Ruby/samplea05.rb
+++ Ruby/samplea05.rb
@@ -1,2 +1,34 @@
 # https://www.fe-siken.com/kakomon/sample20220425/b5.html
 
+class Words
+  def initialize
+    @tango = ["importance", "inflation", "information", "innovation"]
+  end
+
+  def freq(str)
+    cnt = 0.0
+    @tango.each do |x|
+      (x.size - (str.size - 1)).times do |i|
+        if x[i, str.size] == str
+          cnt += 1
+        end
+      end
+    end
+    cnt
+  end
+
+  def freqE(str)
+    cnt = 0.0
+    @tango.each do |x|
+      if x[-1] == str
+        cnt += 1
+      end
+    end
+    cnt
+  end
+end
+
+words = Words.new
+s1 = "n"
+s2 = "f"
+puts words.freq(s1 + s2) / (words.freq("n") - words.freqE("n"))
